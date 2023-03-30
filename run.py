41824bf6f17760dafa7a3389c832bb079092308a
danpopovsky1997 <danielpopovsky1010@gmail.com>
2023-03-30 16:03:18 +0000

    Add prompt to python code, Construct a URL for the openweathermap, create a GET request to the API, create an error message, Parse the response and extract required information, display the weather and temperature data to user

--- run.py
+++ run.py
@@ -1,16 +1,25 @@
 import requests
 
-parameter = "t_2m:C"
-location = input("Enter your desired location(latitude,longitude): ")
-api_key = "#"
+API_KEY = '9a1c17942dd0228bf3921afcac0459e6'
 
-url = f"https://api.meteomatics.com/{parameter}/now/json?latlon={location}&apikey={api_key}"
+# Prompt the user to enter a city name or location.
+user_input = input("Enter a city name or location: ")
 
+# Construct the URL for the OpenWeatherMap API.
+url = f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={API_KEY}"
+
+# Send a GET request to the API and store the response.
 response = requests.get(url)
 
-if response.status_code == 200:
-weather_data = response.json()
-
+# Check if the API returned an error code.
+if response.status_code == 404:
+    print("No city found.")
 else:
-    print("Error: Could not retrieve weather data.")
+    # Parse the response JSON and extract the weather and temperature data.
+    data = response.json()
+    weather = data['weather'][0]['main']
+    temperature = round(data['main']['temp'])
 
+    # Display the weather and temperature data to the user.
+    print(f"The weather in {user_input.title()} is {weather.lower()}.")
+    print(f"The temperature in {user_input.title()} is {temperature}°C.")
