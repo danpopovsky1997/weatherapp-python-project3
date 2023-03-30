e03845f9e53866b285619e9bc6a5e9a5d2ce82b3
danpopovsky1997 <danielpopovsky1010@gmail.com>
2023-03-30 14:49:54 +0000

    Initial commit, import requests

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+location = input("Enter your desired location(post or city name): ")
+api_key = "#"
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+weather_data = response.json()
+
+else:
+    print("Error: Could not retrieve weather data.")
+
